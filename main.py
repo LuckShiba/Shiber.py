cdf8a8caa3afdc346c0b9c131db41608d48131e3
LuckShiba <LuckShiba@protonmail.com>
2020-08-13 22:27:25 -0300

    Fixing some bugs.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from mongoengine import connect
 from os.path import split
 from discord.ext import commands
-from os import environ, walk, listdir
+from os import environ, walk
 from shiber.utils.prefix import get_prefix
+from pathlib import PurePath
 
 EMBED_COLOR = 0xf3c478
 
@@ -16,13 +17,13 @@ class Shiber(commands.Bot):
     def load_extensions(self, extensions_path):
         for path, subdir, files in walk(extensions_path):
             for file in files:
-                if file.endswith('.py'):
-                    self.load_extension('.'.join((*filter(None, split(path)), file[:-3])))
+                if file.endswith('.py') and not file.startswith('__'):
+                    self.load_extension('.'.join((*filter(None, PurePath(path).parts), file[:-3])))
         return
 
 
 if __name__ == '__main__':
     shiber = Shiber()
     connect('shiber', host=environ['MONGO_CONNECTION_STRING'])
-    shiber.load_extensions('cogs')
+    shiber.load_extensions('shiber/cogs')
     shiber.run(environ['TOKEN'])
